cbc4af6efceae3ba7bbed86c6fe01754be09b5d4
Lawrence Jones <git@lwjon.es>
2022-03-04 21:27:24 -0800

    Prompt front matter and manager info

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hi Mom!");
+const inquirer = require("inquirer");
+
+const Manager = require("./lib/Manager");
+const Engineer = require('./lib/Engineer');
+const Intern = require("./lib/Intern");
+const PromptManager = require("./lib/PromptManager");
+
+
+function init() {
+
+  console.log("TEAM DASHBOARD GENERATOR\n");
+  console.log("Start with the team manager.")
+
+  const prompt = new PromptManager();
+  prompt.initializePrompts();
+
+};
+
+
+init();
