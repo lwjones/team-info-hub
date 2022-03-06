eae3b1cf67372208427a3a0504eaffa8bffe2187
Lawrence Jones <git@lwjon.es>
2022-03-05 17:54:56 -0800

    Refactor prompts for access outside of class

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const Intern = require("./lib/Intern");
 const PromptManager = require("./lib/PromptManager");
 
 
-async function init() {
+function init() {
   const prompt = new PromptManager();
 
   console.log("TEAM DASHBOARD GENERATOR\n");
   console.log("Start with the team manager.")
 
   prompt.initializePrompts();
-  console.log(prompt.getEmployees());
+
+  let employeeList = prompt.getEmployees();
+  console.table(employeeList);
 };
 
 
